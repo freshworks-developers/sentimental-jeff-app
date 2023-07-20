bd12578c5907862662c8c6eea6678c2c879ff875
Raviraj Subramanian <raviraj.subramanian@freshworks.com>
2023-07-20 20:13:54 +0530

    [DEVREL-2943] Updated to pfv2.3

--- config/assets/iparams.js
+++ config/assets/iparams.js
@@ -5,50 +5,52 @@ app.initialized().then(client => {
 }, error => {
   console.error('Error: Failed to init the app.')
   console.error(error);
-}
-);
+});
 
 function fillTicketFieldsOptions() {
   let timeout;
   return new Promise(function (resolve, reject) {
     clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      client.iparams.get().then(iparams => {
+    timeout = setTimeout(async function () {
+      try {
+        const iparams = await client.iparams.get();
         const domain = utils.get('domainName') || iparams.domainName;
         const apiKey = utils.get('apiKey') || iparams.apiKey;
         if (domain && apiKey) {
-          var url = `https://${domain}.freshdesk.com/api/v2/ticket_fields`;
-          var options = {
-            headers: {
-              Authorization: 'Basic ' + btoa(apiKey + ':X')
-            }
-          };
-          client.request.get(url, options).then(
-            function (data) {
-              const ticketFields = JSON.parse(data.response);
-              const customFields = ticketFields.filter(field => field.type === 'custom_text').map(filteredField => filteredField.name);
-              if (customFields.length) {
-                utils.set('sentimentField', { disabled: false });
-                utils.set('sentimentField', { values: customFields });
-              } else {
-                console.info('Create a custom text field to update the sentiment result of the ticket to the ticket field.')
-                client.interface.trigger("showNotify", {
-                  type: "info",
-                  message: "To store the sentiment result in a ticket field, create a custom text field to configure here."
-                })
+          try {
+            let result = await client.request.invokeTemplate("getTicketFields", {
+              context: {
+                domain: domain,
+                encodedApiKey: btoa(apiKey + ':X')
               }
-              resolve();
-            },
-            function (error) {
-              console.error('error');
-              console.error(error);
-              reject("Error: Cannot fetch ticket fields.");
             });
+            let ticketFields = JSON.parse(result.response);
+            const customFields = ticketFields.filter(field => field.type === 'custom_text').map(filteredField => filteredField.name);
+            if (customFields.length) {
+              utils.set('sentimentField', { disabled: false });
+              utils.set('sentimentField', { values: customFields });
+            } else {
+              console.info('Create a custom text field to update the sentiment result of the ticket to the ticket field.')
+              client.interface.trigger("showNotify", {
+                type: "info",
+                message: "To store the sentiment result in a ticket field, create a custom text field to configure here."
+              })
+            }
+            resolve();
+          } catch (error) {
+            console.error('error');
+            console.error(error);
+            reject("Error: Cannot fetch ticket fields.");
+          }
         } else {
           console.info('skipped due to empty domain or api key');
           resolve();
         }
-      });
+      } catch (error) {
+        console.error('Error: Cannot get installation parameters');
+        console.error(error);
+        reject('Error: Cannot get installation parameters');
+      }
     }, 500);
   });
 }
